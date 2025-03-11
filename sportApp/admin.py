65a3cae59cdbd526e52540db056861961a32285d
Illia Salohub <eramcheg@gmail.com>
2025-03-11 10:17:43 +0100

    Update: * Enhanced admin panel (Added all models)

--- sportApp/admin.py
+++ sportApp/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 
-from sportApp.models import Tournament, Season, Player, Match, Score, Statistic, Team
-
+from sportApp.models import (
+    Category, UniqueTournament, Tournament, Season,
+    Player, Team, Match, MatchSet, MatchStatistics
+)
 # Register your models here.
 admin.site.register(Player)
 admin.site.register(Season)
 admin.site.register(Tournament)
 admin.site.register(Match)
-admin.site.register(Score)
-admin.site.register(Statistic)
+admin.site.register(Category)
+admin.site.register(UniqueTournament)
 admin.site.register(Team)
+admin.site.register(MatchSet)
+admin.site.register(MatchStatistics)
+
